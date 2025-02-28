2dd44ed567f667255fb00bfd9e5c91e51650ceed
温清洁 <wqjuser@gmail.com>
2025-02-28 19:42:47 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/zpix.ttf',
+  src: '../public/fonts/zpix_round.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
